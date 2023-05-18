3587ea3b9597e3e1f78dbee296f1d419b227a487
Edgars Bērziņš <berzins.edgars.1992@gmail.com>
2023-05-18 09:29:43 +0300

    seminar 2. firs commit

---
+++ src/main/java/com/example/demo/PiSeminar2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiSeminar2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiSeminar2Application.class, args);
+	}
+
+}
