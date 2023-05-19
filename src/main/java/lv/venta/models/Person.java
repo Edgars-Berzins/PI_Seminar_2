b71ad06596c9b980dfced8a5d4b35db6190b4852
Edgars Bērziņš <berzins.edgars.1992@gmail.com>
2023-05-19 10:10:13 +0300

    perosn class

---
+++ src/main/java/lv/venta/models/Person.java
@@ -0,0 +1,42 @@
+package lv.venta.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Person {
+	
+	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Idpe") // DB pusē izveidosies kolonna "id" un būs kā auto increment PK
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idp;
+
+	@Column(name = "Name") // DB pusē izveidosies kolonna "Name"
+	@NotNull
+	@Pattern(regexp = "[A-Z]{1}[a-zēūīļķšāžčņ\\ ]+", message = "Pirmajam burta mjābūt lielajam")
+	@Size(min = 3, max = 30)
+	private String name;
+
+	@Column(name = "Surname") // DB pusē izveidosies kolonna "Surname"
+	@NotNull
+	@Pattern(regexp = "[A-Z]{1}[a-zēūīļķšāžčņ\\ ]+", message = "Pirmajam burta mjābūt lielajam")
+	@Size(min = 3, max = 30)
+	private String surname;
+	
+}
